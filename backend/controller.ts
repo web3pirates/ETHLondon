3317ef6102c8da89305e7295b5a621ba3cffd6b1
Christian Kobril <christiankobril@gmail.com>
2024-03-16 09:09:35 +0000

    chore: handle case in which the user already exists

--- backend/controller.ts
+++ backend/controller.ts
@@ -3,6 +3,14 @@ import User from "./models/user";
 
 export async function handleFarcasterLogin(req: Request, res: Response) {
   try {
+    const { farcasterId } = req.body;
+
+    const alreadyExisting = await User.findOne({ farcasterId });
+
+    if (alreadyExisting) {
+      return res.status(400).send("User already exists");
+    }
+
     const user = await User.create(req.body);
     res.json(user);
   } catch (error: any) {
