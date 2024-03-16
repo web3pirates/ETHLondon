661e15cfb04285d2f859a0cd15a69339b6b68677
Christian Kobril <christiankobril@gmail.com>
2024-03-16 20:32:14 +0000

    chore: revert old frame

--- frontend/src/pages/quest/[id].tsx
+++ frontend/src/pages/quest/[id].tsx
@@ -1,51 +1,29 @@
-import { getFrameMetadata, FrameMetadata } from "@coinbase/onchainkit";
+import {
+  getFrameMetadata,
+  FrameMetadata,
+  FrameButtonMetadata,
+  FrameImageMetadata,
+} from "@coinbase/onchainkit";
 import type { Metadata } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useAsyncMemo } from "use-async-memo";
 
-// const frameMetadata = getFrameMetadata({
-//   buttons: [
-//     {
-//       label: "Send tx",
-//     },
-//   ],
-//   image: `${process.env.NEXT_PUBLIC_URL}/main.png`,
-//   post_url: `${process.env.NEXT_PUBLIC_URL}/api/account`,
-// });
-
-// console.log(frameMetadata);
-
-// export const metadata: Metadata = {
-//   title: "Testing",
-//   description: "LFG",
-//   openGraph: {
-//     title: "Testing",
-//     description: "LFG",
-//     images: [`${process.env.NEXT_PUBLIC_URL}/main.png`],
-//   },
-//   other: {
-//     ...frameMetadata,
-//   },
-// };
-
 export default function Page() {
-  const [isFetching, setIsFetching] = useState(true);
-
   return (
     <>
       <Head>
         <FrameMetadata
           buttons={[
             {
-              label: "Complete task link",
-              action: "link",
-              target: "https://uniswap.org",
+              label: "Send tx",
+              action: "tx",
+              target: `${process.env.NEXT_PUBLIC_URL}/api/quest/1/execute`,
             },
             {
               action: "post_redirect",
-              label: "Verify",
+              label: "Leaderboard",
             },
             {
               action: "link",
@@ -55,12 +33,12 @@ export default function Page() {
           ]}
           image={{
             src: "https://zizzamia.xyz/park-3.png",
-            aspectRatio: "1:1",
+            aspectRatio: "1.91:1",
           }}
           state={{
             counter: 1,
           }}
-          postUrl="http://localhost:3001/api/leaderboard"
+          postUrl={`${process.env.NEXT_PUBLIC_URL}/api/leaderboard`}
         />
       </Head>
       My cool frame id
