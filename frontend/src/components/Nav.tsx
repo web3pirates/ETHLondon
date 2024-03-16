f08b20c7ca83d0fdc2442f49b17ea1eaf7ef01dc
Riksa01 <riccardobusiness04@gmail.com>
2024-03-16 16:02:21 +0000

    button adjusted

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,29 +1,23 @@
 import Menu from "./Menu";
 import Wrapper from "./Wrapper";
-import { useSharedState } from "@/utils/store";
+import { handleSignInWithFarcaster } from "@/utils/signInWithFarcaster";
+import { SignInButton } from "@farcaster/auth-kit";
+import "@farcaster/auth-kit/styles.css";
 
 export function Nav() {
-  const [{ user }] = useSharedState();
-
   return (
-    <Wrapper>
-      <Menu />
-      <div style={{ display: "flex", gap: "10px" }}>
-        {user && (
-          <button
-            className={`text-gray-500 hover:text-gray-900`}
-            style={{ display: "flex", gap: "10px", alignItems: "center" }}
-          >
-            <img
-              src="/images/github.png"
-              alt={`Github image`}
-              width={15}
-              height={15}
-            />
-            {user.login}
-          </button>
-        )}
-      </div>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Menu />
+        <div className="absolute right-0 top-3 p-5">
+          <SignInButton
+            onSuccess={async ({ fid, username }) => {
+              if (!fid || !username) return;
+              await handleSignInWithFarcaster(fid, username);
+            }}
+          />
+        </div>
+      </Wrapper>
+    </>
   );
 }
