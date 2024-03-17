4bacbec2539be32b5b9dc023869f6e82d11e05fb
MicheleSottocasa <michelesottocasa1@gmail.com>
2024-03-17 00:12:39 +0000

    chore: adding router

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -3,11 +3,13 @@ import { useSharedState } from "@/utils/store";
 import { SignInButton } from "@farcaster/auth-kit";
 import "@farcaster/auth-kit/styles.css";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export function Nav() {
   const [{ farcasterId }, dispatch] = useSharedState();
+  const router = useRouter();
 
   const handleSignInWithFarcaster = async (fid: number, username: string) => {
     if (farcasterId) return;
@@ -26,14 +28,12 @@ export function Nav() {
   return (
     <>
       <Wrapper>
-<<<<<<< HEAD
         <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
-=======
-        <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
->>>>>>> 5d730fbdf411beb461571168629c2d1deb9989bf
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a
-              href="https://flowbite.com/"
+            <button
+              onClick={() => {
+                router.push("/");
+              }}
               className="flex items-center space-x-3 rtl:space-x-reverse"
             >
               <Image
@@ -42,14 +42,10 @@ export function Nav() {
                 height={50}
                 alt="My Logo"
               />
-<<<<<<< HEAD
               <span className="self-center text-2xl font-semibold whitespace-nowrap ">
-=======
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
->>>>>>> 5d730fbdf411beb461571168629c2d1deb9989bf
                 Frame n' Chips
               </span>
-            </a>
+            </button>
             <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
               <SignInButton
                 onSuccess={({ fid, username }) => {
@@ -60,11 +56,7 @@ export function Nav() {
               <button
                 data-collapse-toggle="navbar-sticky"
                 type="button"
-<<<<<<< HEAD
                 className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-=======
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
->>>>>>> 5d730fbdf411beb461571168629c2d1deb9989bf
                 aria-controls="navbar-sticky"
                 aria-expanded="false"
               >
@@ -90,35 +82,27 @@ export function Nav() {
               className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
               id="navbar-sticky"
             >
-<<<<<<< HEAD
               <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
                 <li>
-                  <a
-                    href="/"
+                  <button
+                    onClick={() => {
+                      router.push("/");
+                    }}
                     className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
-=======
-              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                  <a
-                    href="/"
-                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
->>>>>>> 5d730fbdf411beb461571168629c2d1deb9989bf
                     aria-current="page"
                   >
                     Home
-                  </a>
+                  </button>
                 </li>
                 <li>
-                  <a
-                    href="/create"
-<<<<<<< HEAD
+                  <button
+                    onClick={() => {
+                      router.push("/create");
+                    }}
                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
-=======
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
->>>>>>> 5d730fbdf411beb461571168629c2d1deb9989bf
                   >
                     Create frame
-                  </a>
+                  </button>
                 </li>
               </ul>
             </div>
